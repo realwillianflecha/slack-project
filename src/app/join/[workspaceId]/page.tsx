2d1e67b7f38b8aecafa33d1659af3838ab53a1d8
realwillianflecha <ramonflechaduarte@gmail.com>
2025-12-12 15:19:36 -0300

    anadir comentario

--- src/app/join/[workspaceId]/page.tsx
+++ src/app/join/[workspaceId]/page.tsx
@@ -44,7 +44,7 @@ const JoinPage = ({ params }: JoinPageProps) => {
             </div>
         )
     }
-
+// anadir comentario
     return ( 
         <div className="h-full flex flex-col gap-y-8 items-center justify-center bg-white p-8 rounded-lg shadow-md">
             <Image src="/hashtag.svg" width={60} height={60} alt="Hashtag" />
